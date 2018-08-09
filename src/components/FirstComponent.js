bb6bde09be2ec1ed8213a9e6a3db0ef0cb8f599b
Leo Gisselberg <leo.gisselberg@digitalspark.se>
2018-08-09 10:42:28 +0200

    DSW-002 Create-landing page

---
+++ src/components/FirstComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default () => {
+    return (
+        <div className="component first-component">
+            <div className="container">
+                <h1>DigitalSpark</h1>
+            </div>
+        </div>
+    )
+}
