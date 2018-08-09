b30ce1b2ff525a2960bec6c0b44d5fd4b0705034
Leo Gisselberg <leo.gisselberg@digitalspark.se>
2018-08-09 14:23:07 +0200

    DSW-003 About page created, had to change scroll package. Placeholder font also added

--- src/components/SecondComponent.js
+++ src/components/SecondComponent.js
@@ -3,7 +3,21 @@ import React from "react";
 export default () => {
     return (
         <div className="component second-component">
-            <h2>Second Component</h2>
+            <div className="container-fluid row">
+                <div className="col-lg-6 col-md-12 col-sm-12">
+                    <h1 className="about-title">Om DigitalSpark</h1>
+                    <p className="about-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Maecenas sed suscipit ante. Sed iaculis tincidunt sem, eget consectetur est fermentum in. 
+                    Duis sed odio quis quam vestibulum suscipit. Mauris ut vehicula orci, vitae vehicula neque. 
+                    Aenean finibus et risus a cursus. Mauris elementum hendrerit ligula quis scelerisque. 
+                    Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+                    ivamus magna metus, venenatis id vulputate quis, sagittis in diam.
+                    </p>
+                </div>
+                <div className="col-lg-6 col-md-12 col-sm-12">
+                    <img src="https://d39l2hkdp2esp1.cloudfront.net/img/photo/139486/139486_tt.jpg?20180131020112" className="about-img"></img>
+                </div>
+            </div>
         </div>
     )
 }
