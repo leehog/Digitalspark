be1ad32f628030761464ed78fdfb0a4313c35c36
Leo Gisselberg <leo.gisselberg@digitalspark.se>
2018-08-10 09:56:17 +0200

    DSW-004 Jobs page created

---
+++ src/components/ThirdComponent.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default () => {
+    return (
+        <div className="component third-component">
+            <div className="container-fluid">
+                <h1 className="about-title">Vad gör vi?</h1>
+                <div className="col-lg-12 job-wrap">
+                    <h2 className="job-title">IT Consultant</h2>
+                    <p className="job-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Praesent nulla ipsum, euismod sed consequat eget, luctus id neque. Sed eu lacus turpis. 
+                    Suspendisse a laoreet ante. Nulla rutrum sed nisl laoreet convallis. 
+                    Quisque posuere purus nec tristique posuere. Suspendisse nisl purus, rhoncus in pulvinar eu, convallis in ligula.
+                     Duis dui sem, ultricies non ligula quis, tincidunt tempor tellus. Sed viverra risus a iaculis elementum. 
+                     Duis id tellus ut sem lobortis fringilla suscipit posuere nunc. Praesent imperdiet libero consectetur nisl consectetur commodo.
+                    </p>
+                    <div className="read-more-btn">Läs mer</div>
+                </div>
+                <div className="col-lg-12 job-wrap">
+                <h2 className="job-title">Sales</h2>
+                <p className="job-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Praesent nulla ipsum, euismod sed consequat eget, luctus id neque. Sed eu lacus turpis. 
+                    Suspendisse a laoreet ante. Nulla rutrum sed nisl laoreet convallis. 
+                    Quisque posuere purus nec tristique posuere. Suspendisse nisl purus, rhoncus in pulvinar eu, convallis in ligula.
+                     Duis dui sem, ultricies non ligula quis, tincidunt tempor tellus. Sed viverra risus a iaculis elementum. 
+                     Duis id tellus ut sem lobortis fringilla suscipit posuere nunc. Praesent imperdiet libero consectetur nisl consectetur commodo.
+                    </p>
+                    <div className="read-more-btn">Läs mer</div>
+                </div>
+            </div>
+        </div>
+    )
+}
