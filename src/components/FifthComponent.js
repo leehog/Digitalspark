0460cbbaa19b3cfa74b76c45e3fa3fe96b22acd0
Leo Gisselberg <leo.gisselberg@digitalspark.se>
2018-08-10 13:42:36 +0200

    Contact done, started footer

---
+++ src/components/FifthComponent.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default () => {
+    return (
+        <div className="component fifth-component">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-lg-4 col-md-4 col-sm-12">
+                        <h4>contact@digitalspark.se</h4>
+                    </div>
+                    <div className="col-lg-4 col-md-4 col-sm-12 contact-borders">
+                        <h4>Dalslandsgatan 3</h4>
+                    </div>
+                    <div className="col-lg-4 col-md-4 col-sm-12">
+                        <h4>+46701230419</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
