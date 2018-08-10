2e9a79176e7c5ddaf5c0472c8d673c7d658a8780
Leo Gisselberg <leo.gisselberg@digitalspark.se>
2018-08-10 11:51:03 +0200

    DSW-005 Our Consultants page done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import FirstComponent from "./components/FirstComponent";
 import SecondComponent from "./components/SecondComponent";
 import ThirdComponent from "./components/ThirdComponent";
+import FourthComponent from "./components/FourthComponent";
 import * as Scroll from 'react-scroll';
 import { Link, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
@@ -15,11 +16,14 @@ class App extends Component {
       <Link activeClass="active" className="scroll-link" to="first-component" spy={true} smooth={true} duration={500} >Start</Link>
       <Link activeClass="active" className="scroll-link" to="second-component" spy={true} smooth={true} duration={500} >About us</Link>
       <Link activeClass="active" className="scroll-link" to="third-component" spy={true} smooth={true} duration={500} >Vad vi gör?</Link>
+      <Link activeClass="active" className="scroll-link" to="fourth-component" spy={true} smooth={true} duration={500} >Our Consultants</Link>
+
 
       <React.Fragment>
                 <FirstComponent/>
                 <SecondComponent/>
                 <ThirdComponent/>
+                <FourthComponent/>
         </React.Fragment>
       </div>
     );
